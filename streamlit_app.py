0dd49d1f883fa0c80ace7b6e4a9aab2936809193
sedgwickcspence <151062950+sedgwickcspence@users.noreply.github.com>
2024-03-19 18:53:12 +0000

    Update streamlit_app.py
    added searching capability

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,8 +25,9 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT
 
 # Convert the snowpark dataframe to pandas dataframe so we can use the LOC function
 pd_df = my_dataframe.to_pandas()
-st.dataframe(pd_df)
-st.stop()
+#st.dataframe(pd_df)
+#st.stop()
+
 
 ingredients_list = st.multiselect(
     'Choose up to 5 ingredients: ',
@@ -37,6 +38,8 @@ ingredients_list = st.multiselect(
 if ingredients_list:
 
     ingredients_string = ''
+    search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+    st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
 
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
