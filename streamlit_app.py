8763913b8a240a909bb5cc7a5327b785a29efe37
sedgwickcspence <151062950+sedgwickcspence@users.noreply.github.com>
2024-03-19 17:11:50 +0000

    Update streamlit_app.py
    added json method to requests

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,7 +33,7 @@ if ingredients_list:
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
 
-    #st.write(ingredients_string)
+    #st.write(ingredients_string.json())
 
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
                      values('""" + ingredients_string + """', '"""+name_on_order+"""') """
